bd603259fde3f5a836fdd7e03f36ce5c8ecffdc7
Hông Duc TA <hong-duc.ta@protonmail.com>
2019-03-12 17:24:59 +0100

    feat: added connection to Mongo Atlas

--- app.js
+++ app.js
@@ -3,13 +3,23 @@ const BodyParser = require("body-parser");
 const MongoClient = require("mongodb").MongoClient;
 const ObjectId = require("mongodb").ObjectID;
 
-const CONNECTION_URL = "mongodb+srv://hongducta:<GuS*x/3#v'R7u/xc'RUq>@hongducta-movies-eevor.mongodb.net/test?retryWrites=true"
-const DATABASE_NAME = "hongducta";
+/* local database
+const CONNECTION_URL = "mongodb://localhost:27017";
+const DATABASE_NAME = "denzel";
+*/
+
+const uri = "mongodb+srv://hongducta:<password>@hongducta-movies-eevor.mongodb.net/test?retryWrites=true";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
 
 var app = Express();
 
 const imdb = require('./src/imdb');
-const DENZEL_IMDB_ID = 'nm0000243';
+const DENZEL_IMDB_ID = 'nm0000243'; // id of Denzel. Do not change
 
 const fs = require('fs');
 
@@ -19,7 +29,8 @@ async function fetchFilmography (actor) {
     const movies = await imdb(actor);
     const awesome = movies.filter(movie => movie.metascore >= 70);
 
-    const file = fs.writeFileSync('filmography.json',JSON.stringify(awesome,null,2));
+    fs.writeFileSync('filmography.json',JSON.stringify(awesome,null,2));
+    console.log("Done !");
     process.exit(0);
   } catch (e) {
     console.error(e);
@@ -27,6 +38,20 @@ async function fetchFilmography (actor) {
   }
 }
 
+/* BOUT DE CODE POUR MongoDB CONNECT
+MongoClient.connect(CONNECTION_URL,function(err,client){
+  console.log("Connection success");
+  const db = client.db(DATABASE_NAME);
+  client.close();
+});
+*/
+
+//fetchFilmography(DENZEL_IMDB_ID);
+
+
+
+// BOUT DE CODE POUR API
+/*
 app.listen(9292, () => {
     console.log("Hello !");
 });
@@ -39,14 +64,15 @@ app.get('/movies', function(req,res){
     res.send('Fetch a random must-watch movie.');
 })
 
-app.get('/movies/:id', function(req,res){
-    res.send('Fetch a specific movie.');
-})
-
 app.get('/movies/search', function(req,res){
     res.send('Get movies search');
 })
 
-app.post('/movies/:id', function(req,res){
-    res.send('Save a watched date and review')
+app.get('/movies/:id', function(req,res){
+    res.send('Fetch a specific movie.' + req.params.id);
 })
+
+app.post('/movies/:id', function(req,res){
+    res.send(req.params.id + '\n' + String(req.query));
+})
+*/
